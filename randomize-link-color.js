93f19148e221269c648c43bbdcdc230d0bc651f5
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-22 09:14:30 -0300

    Randomize Link Color

--- randomize-link-color.js
+++ randomize-link-color.js
@@ -1,1 +1,11 @@
-
+let originalColor;
+$("a.colorful-link").mouseenter(function() {
+    originalColor = $(this).css("background-color");
+    let availableColors = $(".link-colors-wrapper").children().length;
+    let randomNum = Math.floor(Math.random() * availableColors);
+    let selectedColor = $(".link-color").eq(randomNum).css("background-color");
+    $(this).css("background-color", selectedColor);
+  })
+  .mouseleave(function() {
+    $(this).css("background-color", originalColor);
+  });
