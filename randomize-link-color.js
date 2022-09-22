197f61cecf321ca47fa103a1db34f1b8e5be2050
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-22 19:59:15 -0300

    Update to use collection

--- randomize-link-color.js
+++ randomize-link-color.js
@@ -1,9 +1,9 @@
 let originalColor;
 $("a.colorful-link").mouseenter(function() {
     originalColor = $(this).css("background-color");
-    let availableColors = $(".link-colors-wrapper").children().length;
+    let availableColors = $(".colorful-links-list").children().length;
     let randomNum = Math.floor(Math.random() * availableColors);
-    let selectedColor = $(".link-color").eq(randomNum).css("background-color");
+    let selectedColor = $(".colorful-links-item").eq(randomNum).css("background-color");
     $(this).css("background-color", selectedColor);
   })
   .mouseleave(function() {
