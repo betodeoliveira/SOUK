3c5ea1f3f302690da32156cd7a12bf0337f7ea6a
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-28 15:56:32 -0300

    randomize link color

--- randomize-link-color.js
+++ randomize-link-color.js
@@ -1,11 +1,18 @@
-let originalColor;
-$("a.colorful-link").mouseenter(function() {
-    originalColor = $(this).css("background-color");
+let originalTextColor = "#eeeeee";
+let hoverTextColor = "#383838";
+
+$(".colorful-link").each(function (index) {
+  $(this).attr("original-background-color", $(this).css("background-color"));
+});
+
+$(".colorful-link").mouseenter(function() {
     let availableColors = $(".colorful-links-list").children().length;
     let randomNum = Math.floor(Math.random() * availableColors);
     let selectedColor = $(".colorful-links-item").eq(randomNum).css("background-color");
     $(this).css("background-color", selectedColor);
+    $(this).css("color", hoverTextColor);
   })
   .mouseleave(function() {
-    $(this).css("background-color", originalColor);
+    $(this).css("background-color", $(this).attr("original-background-color"));
+    $(this).css("color", originalTextColor);
   });
