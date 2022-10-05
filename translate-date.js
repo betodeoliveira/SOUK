67dc27192597fad5c2cf7d738d113123b1e2ad27
Alberto de Olivira <betodeoliveira@outlook.com>
2022-10-05 16:11:16 -0300

    translate date

---
+++ translate-date.js
@@ -0,0 +1,71 @@
+var dateclass = "translate-date";
+
+$(".pagination_button").on("click", function () {
+  setTimeout(translateDate, 1000);
+});
+
+$(".pagination_page-btn-wrapper").on("click", function () {
+  setTimeout(translateDate, 1000);
+});
+
+$(document).ready(function () {
+  translateDate();
+});
+
+function translateDate() {
+  var wfdc = $("." + dateclass);
+
+  wfdc.each(function () {
+    var wfdctxt = $(this).text();
+
+    var monthsEn = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ];
+
+    var monthsPtbr = [
+      "janeiro",
+      "fevereiro",
+      "março",
+      "abril",
+      "maio",
+      "junho",
+      "julho",
+      "agosto",
+      "setembro",
+      "outubro",
+      "novembro",
+      "sezembro",
+      "segunda",
+      "terça",
+      "quarta",
+      "quinta",
+      "sexta",
+      "sábado",
+      "domingo"
+    ];
+
+    for (var i = 0; i < monthsEn.length; i++) {
+      wfdctxt = wfdctxt.replace(monthsEn[i], monthsPtbr[i]);
+    }
+
+    $(this).text(wfdctxt);
+  });
+}
